7e93bdf41848c944ec974096e267893014a96f48
Qianru <qianru@jos-Air-2.attlocal.net>
2023-04-21 21:52:42 -0400

    update

--- performance_test.py
+++ performance_test.py
@@ -5,29 +5,26 @@ from random import *
 import numpy as np
 
 MAX = 1e3
-SCALE = 1.1
-p = 4
+SCALE = 1.2
+
+q = 2
 try:
-    p = int(sys.argv[1])
+    q = int(sys.argv[1])
 except: pass
+
 n = 10
 # define matrix A and vector b
-
 A = np.random.uniform(0, MAX, (n, n))
 x = np.random.uniform(0, MAX, n)
-print(x)
 
-# b = np.dot(A, x)
-
-d = sum(A[i][i] for i in range(len(A)))
-o = np.sum(A) - d
-t = o / d * SCALE
 for i in range(len(A)):
-     A[i][i] *= t
+    diagnal = A[i][i] 
+    other = sum(A[i]) - diagnal
+    A[i][i] *= other / diagnal * SCALE
 
-N = np.random.choice((-1, 1), A.shape)
-A *= N
-b = A@x
+# randomly multiply 1 or (-1)
+A *= np.random.choice((-1, 1), A.shape)
+b = np.dot(A, x)
 
 in_fname1 = f'input_mat_{n}.txt'
 in_fname2 = f'input_vec_{n}.txt'
@@ -39,10 +36,9 @@ with open(in_fname1, 'w') as f:
 with open(in_fname2, 'w') as f:
     f.write(' '.join(map(str, b)) + '\n')
 
-out_fname = f'output_{p}.txt'
+out_fname = f'output.txt'
 
-
-os.system(f'make && mpirun -np {p} ./pjacobi {in_fname1} {in_fname2} {out_fname}> /dev/null')
+os.system(f'make && mpirun -np {q*q} ./pjacobi {in_fname1} {in_fname2} {out_fname}> /dev/null')
 
 with open(out_fname, 'r') as f:
     prog_out = list(map(float, f.readline().strip().split()))
